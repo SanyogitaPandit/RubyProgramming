9e2206b9cca1bf4ab12ca803aaddd5335b1f3df7
sanyogita <sanyogitapandit1989@gmail.com>
2020-03-17 16:28:34 -0400

    Project Event manager

---
+++ event_manager/lib/event_manager.rb
@@ -0,0 +1,19 @@
+require 'csv'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+
+puts "EventManager initialized."
+
+contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  
+  clean_zipcode(zipcode)
+
+  puts "#{name} #{zipcode}"
+end
